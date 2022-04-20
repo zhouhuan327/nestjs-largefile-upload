37f9c03e17c5c683907fe1eb261b9f34fcb97c30
zhouhuan <zsean21598@163.com>
2022-04-20 17:48:07 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+  await app.enableCors();
+  await app.listen(3000);
+  Logger.log('http://localhost:3000');
+}
+bootstrap();
